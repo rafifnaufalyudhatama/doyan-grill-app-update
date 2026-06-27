f3315b32eac523bdfc483c50180a230e41cc3aa7
Rafif Naufal <rafif.nafy@gmail.com>
2026-06-27 21:21:57 +0700

    Fix form is not secure by forcing HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         view()->composer('*', function ($view) {
             if (request()->is('admin/*')) {
                 return;
